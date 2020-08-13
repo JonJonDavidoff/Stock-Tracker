541231678b5f5f563350d2f2397d6a7620164ec4
Yarden Chen <yardench@gmail.com>
2020-08-13 18:04:39 +0300

    check

--- app.py
+++ app.py
@@ -2,14 +2,30 @@
 """
 hello_flask: First Python-Flask webapp
 """
-from flask import Flask, render_template  # Need render_template() to render HTML pages
-app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+from flask import Flask, render_template, request  # Need render_template() to render HTML pages
 
-@app.route('/')   # URL '/' to be handled by main() route handler
+app = Flask(__name__)  # Construct an instance of Flask class for our webapp
+
+
+@app.route('/')  # URL '/' to be handled by main() route handler
 def main():
     """Say hello"""
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    projectpath = request.form['projectFilepath']
+    print projectpath
+    return render_template('home.html')
+    # your code
+    # return a response
+
+@app.route('/addRegion', methods=['POST'])
+def addRegion():
+    projectpath = request.form['projectFilepath']
+    print(projectpath)
+
 
 if __name__ == '__main__':  # Script executed directly?
-    app.run()
-
+    app.run(debug=True, host="0.0.0.0")
