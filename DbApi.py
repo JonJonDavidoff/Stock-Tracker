1ce8b1e1d22b1232feb0f924afc408a253032086
jon jon davidoff <jonjondavidofff@gmail.com>
2020-08-14 19:23:01 +0300

    Fixed DbApi after I deleted a part

--- DbApi.py
+++ DbApi.py
@@ -278,6 +278,167 @@ def update_stock(ticker, user_id, amount_of_stocks=-1, cost=-1):
             if amount_of_stocks == -1:
                 amount_of_stocks = stock.get_amount_of_stocks()
             else:
+                # TODO create message on ui
+                print("Stock already exists")
+                return False
+    except Exception:
+        print("Error")
+
+
+def get_user_id_by_email(email):
+    """
+    get_user_id_by_email is a function that gets an email and retuns the related user id
+    :param email: the users email
+    :return: the user's id
+    """
+    return get_user_by_email(email).get_id()
+
+
+def add_stock_by_email(email, ticker, cost_of_stock=-1, amount_of_stocks=0):
+    """
+     add_stock_by_email is a function that get the user_id  checks if the stock exists in the  db and if it doesnt exits adds it to db
+    :param email:
+    :param ticker:
+    :param cost_of_stock:
+    :param amount_of_stocks:
+    :return: None
+    """
+    user = get_user_by_email(email)
+    add_stock(ticker=ticker, user_id=user.get_id(), cost_of_stock=cost_of_stock,
+              amount_of_stocks=amount_of_stocks)
+
+
+def get_user_by_email(email):
+    """
+    get_user_by_email is a function that conects to the db and returns the user by the email
+    :param email: the email of the user
+    :return: User object
+    """
+    try:
+        data = execute_select_query(sql="SELECT * FROM dbo.Users WHERE Email='" + email + "'")
+        list = data_to_list(data)
+        user = User.User(user_id=list[0][0], first_name=list[0][1], last_name=list[0][2], email=list[0][3],
+                         password=list[0][4])
+        return user
+    except Exception:
+        print("Error")
+
+
+def get_user_by_id(id):
+    try:
+        data = execute_select_query(sql="SELECT * FROM dbo.Users WHERE id=" + str(id) + "")
+        list = data_to_list(data)
+        user = User.User(user_id=list[0][0], first_name=list[0][1], last_name=list[0][2], email=list[0][3],
+                         password=list[0][4])
+        return user
+    except Exception:
+        print("Error")
+
+
+def data_to_list(data):
+    list = []
+    for row in data:
+        row_to_list = [elem for elem in row]
+        list.append(row_to_list)
+    return list
+
+
+def get_users_stocks_by_user_id(user_id):
+    """
+    get_users_stocks_by_user_id is a function that gets a user id and returns all of the users stocks
+    :param user_id: the user id to search for
+    :return: list of Stocks
+    """
+    # get data from db
+    stock_data = execute_select_query(sql="SELECT * FROM dbo.Stocks WHERE user_id=" + str(user_id))
+    # convert data to list
+    list_of_stocks = data_to_list(stock_data)
+    # move data to list of all stocks
+    return_user_stocks_list = []
+    temp_stock = 0
+    for stock in list_of_stocks:
+        temp_stock = Stock.Stock(ticker=stock[1], amount_of_stocks=stock[2], cost=stock[4])
+        return_user_stocks_list.append(temp_stock)
+    return return_user_stocks_list
+
+
+def get_users_stock_by_ticker(user_id, ticker):
+    try:
+        sql = "SELECT * FROM dbo.Stocks WHERE ticker='" + ticker + "' AND user_id=" + str(user_id)
+        data = execute_select_query(sql)
+        data = data_to_list(data)
+        return Stock.Stock(ticker=data[0][1], amount_of_stocks=data[0][2], cost=data[0][4])
+    except Exception as e:
+        print("Error... " + str(e))
+
+
+def get_users_stocks_by_email(email):
+    """
+     get_users_stocks_by_user_id is a function that gets an email and returns all of the users stocks
+    :param email:
+    :return:
+    """
+    return get_users_stocks_by_user_id(user_id=get_user_id_by_email(email))
+
+
+def print_all_users(self):
+    """
+    print_all_users is a function that prints all users from the Users table
+    :return: None
+    """
+    data = execute_select_query("SELECT * FROM dbo.Users")
+    print("[id   FirstName   LastName   Email                             Password]")
+    for row in data:
+        print(row)
+
+
+def print_all_stocks(self):
+    """
+    print_all_users is a function that prints all stocks from the Stocks table
+    :return: None
+    """
+    data = execute_select_query("SELECT * FROM dbo.Stocks")
+    print("[id   ticker   amount_of_stocks   user_id cost]")
+    for row in data:
+        print(row)
+
+
+def remove_stock_by_user_id(ticker, user_id):
+    """
+    remove_stock_by_user_id is a function that removes a stocks by the user's id
+    :param ticker: the stocks ticker
+    :param user_id: the user's id
+    :return: None
+    """
+    execute_query(sql="DELETE FROM dbo.Stocks WHERE ticker='" + ticker + "' AND user_id=" + str(user_id))
+
+
+def remove_user(user_id):
+    """
+      remove_stock_by_user_id is a function that removes a user and its stocks from db by the user's id
+      :param user_id: the user's id
+      :return: None
+      """
+    execute_query(sql="DELETE FROM dbo.Stocks WHERE user_id=" + str(user_id))
+    execute_query("DELETE FROM dbo.Users WHERE id=" + str(user_id))
+
+
+def update_stock(ticker, user_id, amount_of_stocks=-1, cost=-1):
+    """
+    update_stock is a function that gets a ticker, user_id , amount_of_stocks, cost and updates the db accordingly
+    if default value,-1, the default will stay the same
+    :param ticker:
+    :param user_id:
+    :param amount_of_stocks: if default will stay the same values from the db
+    :param cost: if default will stay the same values from the db
+    :return: None
+    """
+    try:
+        if amount_of_stocks == -1 or cost == -1:
+            stock = get_users_stock_by_ticker(ticker=ticker, user_id=user_id)
+            if amount_of_stocks == -1:
+                amount_of_stocks = stock.get_amount_of_stocks()
+            else:
                 cost = stock.get_cost()
 
         execute_query(
@@ -285,6 +446,8 @@ def update_stock(ticker, user_id, amount_of_stocks=-1, cost=-1):
                 cost) + "  WHERE user_id =" + str(user_id) + "  AND ticker = '" + ticker + "'")
     except Exception as e:
         print("Error  " + str(e))
+        cost = stock.get_cost()
+
 
 
 def main():
